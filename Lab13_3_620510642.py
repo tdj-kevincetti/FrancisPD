92f7f5ef7b8928d86be283abb63d3c72ddf32165
Darth Vader <dulyapap_c@cmu.ac.th>
2020-11-16 09:42:26 +0700

    Tu Na Hee

---
+++ Lab13_3_620510642.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# นายดุลยภาพ จินะแปง
+# 620510642
+# Lab 13
+# Problem 3
+# 204111 Sec 003
+import string
+def word_count(text):
+    counts = dict() #สร้างดิกเปล่า
+    text = text.lower() #ปรับให้เป็นตัวเล็ก
+    words = text.split() #แยกคำจากช่องว่าง
+    for word in words:
+        word = word.strip(string.punctuation) #ลบอักษรพิเศษตรงขอบ
+        if word in counts: #มีคำอยู่แล้ว
+            counts[word] += 1 #นับเพิ่ม
+        else: #คำใหม่
+            counts[word] = 1
+    return counts
+def main():
+    print(word_count('the quick brown fox jumps over the lazy dog.'))
+    print(word_count("He doesn't want to pay $40,000 for a new car, but his wife doesn't seem to care, he said."))
+    print(word_count("I'm your father!!!!"))
+    print(word_count("Fuck Call me 555-5555, Marker"))
+    print(word_count("Dumb Prayut Chan-O-Cha"))
+
+if __name__ == '__main__':
+    main()
+    
